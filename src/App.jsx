40892683e1be4e123a4112e4f77bb64fb6d76997
moon0929 <ansquddnr4545@gmail.com>
2026-02-19 18:46:17 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Layout from './layout/layout.jsx'
+import Profile from './pages/Profile.jsx'
+import Skills from './pages/skills.jsx'
+
+function App() {
+
+  return (
+    <Layout>
+      <Profile />
+      <Skills />
+    </Layout>
+  )
+}
+
+export default App
---
+++ src/pages/Skills.jsx
@@ -0,0 +1,11 @@
+import './page_styles.css'
+
+export default function Skills() {
+  return (
+    <section id="skills" className="section">
+      <div className="container sectionInner">
+        <h1 className="sectionTitle">Skills</h1>
+      </div>
+    </section>
+  )
+}
